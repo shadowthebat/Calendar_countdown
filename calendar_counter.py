3aa74cefbebfe1480ca27fdc2ecd7f7c1dff9e93
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2019-06-21 03:56:28 -0400

    add fixed dates

--- calendar_counter.py
+++ calendar_counter.py
@@ -7,10 +7,22 @@ import os
 ny = upcomming(ny)
 bday = upcomming(born)
 canada = upcomming(can)
+kbd = upcomming(kbd)
+mbd = upcomming(mbd)
+stj = upcomming(stj)
+labour = upcomming(labour)
+thanks = upcomming(thanks)
+hallow = upcomming(hallow)
+remember = upcomming(remember)
+xmas = upcomming(xmas)
+valen = upcomming(valen)
+stpat = upcomming(stpat)
 
 
 os.system('clear')
+os.system('clear')
 master = {}
+
 # create and add countdowns to master dictionary
 bday = D('birthday', bday)
 master[bday.dist.days] = bday
@@ -21,6 +33,33 @@ master[ny.dist.days] = ny
 canada = D('canada day', can)
 master[canada.dist.days] = canada
 
-# prints countdowns from soonest to latest
-for i in sorted(master.keys()):
+kbd = D('kaya', kbd)
+master[kbd.dist.days] = kbd
+
+mbd = D('mommy', mbd)
+master[mbd.dist.days] = mbd
+
+stj = D('st. jean', stj)
+master[stj.dist.days] = stj
+'''
+labour = D('labour day', labour)
+master[labour.dist.days] = labour
+
+thanks = D('thanks giving', thanks)
+master[thanks.dist.days] = thanks
+'''
+hallow = D('halloween', hallow)
+master[hallow.dist.days] = hallow
+
+remember = D('rememberance day', remember)
+master[remember.dist.days] = remember
+
+xmas = D('christmas', xmas)
+master[xmas.dist.days] = xmas
+
+stpat = D('st patricks day', stpat)
+master[stpat.dist.days] = stpat
+
+# prints countdowns in chronological order
+for i in sorted(master.keys(), reverse=True):
     master[i].display()
