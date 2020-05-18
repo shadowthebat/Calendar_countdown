1f8c71fabd18292b36102b1ad46976aad2252e17
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2020-05-17 22:56:44 -0400

    -r is buggy

--- calendar_fresh.py
+++ calendar_fresh.py
@@ -23,14 +23,21 @@ elif len(sys.argv) == 2 and sys.argv[1] == 'len':
 elif len(sys.argv) == 5 and sys.argv[1] == '-a':
     # add event to calendar with given date
     add_event_by_date(sys.argv[2], sys.argv[3], sys.argv[4])
+    print(f'Event added: {sys.argv[2]}')
 
 elif len(sys.argv) == 4 and sys.argv[1] == '-t':
     # add event to calendar x_num days away
     add_event_by_dist_days(sys.argv[2], sys.argv[3])
+    print(f'Event added: {sys.argv[2]}')
+
+''' REMOVE FUNCTION BUGS
 
 elif len(sys.argv) == 3 and sys.argv[1] == '-r':
     # add event to calendar x_num days away
     remove_event(sys.argv[2])
+    print(f'Event removed: {sys.argv[2]}')
+
+'''
 
 else:
     # prints calendar in chronological order
